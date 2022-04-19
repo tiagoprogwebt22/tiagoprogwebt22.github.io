1357c7bc23327c4aee3f82ae2fc00193da69af23
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-19 15:17:42 -0300

    Programação do CEP e Endereço
    Uso de ajax com a função fetch e da lib VMasker para máscaras de telefone e CEP.

---
+++ simplicity/js/scripts.js
@@ -0,0 +1,58 @@
+/* JS INICIAL PARA CEP/ENDEREÇO */
+const formulario = document.querySelector("form");
+const inputCep = formulario.querySelector("#cep");
+const inputTelefone = formulario.querySelector("#telefone");
+const inputEndereco = formulario.querySelector("#endereco");
+const inputBairro = formulario.querySelector("#bairro");
+const inputCidade = formulario.querySelector("#cidade");
+const inputEstado = formulario.querySelector("#estado");
+const bStatus = formulario.querySelector("#status");
+const botaoLocalizar = formulario.querySelector("#localizar-cep");
+
+botaoLocalizar.addEventListener("click", function(event){
+    event.preventDefault();
+    // Entre no site: viacep.com.br
+
+    /* Pegar o cep digitado */
+    let cep = inputCep.value;
+
+    /* CEP no padrão da API */
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+    //let url = "https://viacep.com.br/ws/"+cep+"/json/";
+
+    /* Ajax: comunicação assíncrona com
+    o ViaCEP usando a função chamada fetch. */
+
+    // 1) Fazer a conexão com a API (ou acessar)
+    fetch(url)
+
+        // 2) E então, recupere a resposta do acesso no formato JSON
+        .then(resposta => resposta.json())
+        
+            // 3) E então, mostre os dados
+            .then(dados => {
+                if ( "erro" in dados ) {
+                    bStatus.innerHTML = "CEP não encontrado!";
+                    inputCep.focus();
+                } else {
+                    bStatus.innerHTML = "CEP encontrado!";
+                    inputEndereco.value = dados.logradouro;                    
+                    inputBairro.value = dados.bairro;
+                    inputCidade.value = dados.localidade;
+                    inputEstado.value = dados.uf;
+                }
+            });
+});
+
+
+/* Lib VanillaMasker:
+https://github.com/vanilla-masker/vanilla-masker */
+VMasker(inputCep).maskPattern("99999-999");
+VMasker(inputTelefone).maskPattern("(99) 9999-9999");
+
+
+
+
+
+
+
