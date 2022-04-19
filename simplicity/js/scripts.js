51f56fbca27e4121563baccd38046854fcf712d8
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-19 16:34:03 -0300

    Contador de caracteres

--- simplicity/js/scripts.js
+++ simplicity/js/scripts.js
@@ -51,6 +51,39 @@ VMasker(inputCep).maskPattern("99999-999");
 VMasker(inputTelefone).maskPattern("(99) 9999-9999");
 
 
+/* Programação do contador de caracteres
+do campo mensagem */
+const spanMaximo = formulario.querySelector("#maximo");
+const bCaracteres = formulario.querySelector("#caracteres");
+const textMensagem = formulario.querySelector("#mensagem");
+
+// Determinar a quantidade máxima de caracteres
+let quantidade = 100;
+
+// Evento para detectar a digitação (entrada) no campo
+textMensagem.addEventListener("input", function(){
+    
+    // Capturando o que for digitado
+    let conteudo = textMensagem.value;
+
+    // Criando uma contagem regressiva
+    let contagem = quantidade - conteudo.length;
+
+    // Adicionando a contagem ao elemento HTML
+    bCaracteres.textContent = contagem;
+
+    if (contagem == 0) {
+        bCaracteres.style.color = "red";
+        textMensagem.style.boxShadow = "red 0 0 10px";
+    } else {
+        bCaracteres.style.color = "black";
+        textMensagem.style.boxShadow = "black 0 0 10px";
+    }
+});
+
+
+
+
 
 
 
