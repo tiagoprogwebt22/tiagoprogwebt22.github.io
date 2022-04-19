8aa00d799c1a53a22f33413001a305112387276f
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-19 17:04:57 -0300

    Aplicação da API formspree

--- simplicity/js/scripts.js
+++ simplicity/js/scripts.js
@@ -84,6 +84,41 @@ textMensagem.addEventListener("input", function(){
 
 
 
+/* Programação do Formspree */
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Obrigado por enviar!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! Deu ruim! Tente novamente mais tarde"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+
+
+
 
 
 
